840da07912f155f2d2e896f4932c137f6fdb51fa
khiemdev <khiembi161219@gmail.com>
2024-06-20 08:50:01 +0700

    Form submission

---
+++ app/Http/Controllers/IdeaController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Idea;
+use Illuminate\Http\Request;
+
+class IdeaController extends Controller
+{
+    public function store(Request $request)
+    {
+        $idea = Idea::create([
+            'content' => $request->idea,
+        ]);
+        return redirect()->route('dashboard');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\IdeaController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,7 +14,8 @@ use App\Http\Controllers\ProfileController;
 |
 */
 
-Route::get('/', [DashboardController::class, 'index']);
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::post('/idea', [IdeaController::class, 'store'])->name('idea.store');
 
 Route::get('/terms', function () {
     return view('terms');
