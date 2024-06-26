c780b5e35c8d6a3ba4f07a19cb0b248524619cf5
khiemdev <khiembi161219@gmail.com>
2024-06-26 10:06:54 +0700

    Resource Routing

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,19 @@ use App\Http\Controllers\AuthController;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::group(['prefix' => 'ideas', 'as' => 'ideas.', 'middleware' => 'auth'], function () {
-    Route::post('', [IdeaController::class, 'store'])->name('store')->withoutMiddleware('auth');
-    Route::get('/{idea}', [IdeaController::class, 'show'])->name('show')->withoutMiddleware('auth');
-    Route::get('/{idea}/edit', [IdeaController::class, 'edit'])->name('edit');
-    Route::put('/{idea}/update', [IdeaController::class, 'update'])->name('update');
-    Route::delete('/{idea}', [IdeaController::class, 'destroy'])->name('destroy');
-    Route::post('/{idea}/comments', [CommentController::class, 'store'])->name('comment.store');
-});
+//Route::group(['prefix' => 'ideas', 'as' => 'ideas.', 'middleware' => 'auth'], function () {
+//    Route::post('', [IdeaController::class, 'store'])->name('store')->withoutMiddleware('auth');
+//    Route::get('/{idea}', [IdeaController::class, 'show'])->name('show')->withoutMiddleware('auth');
+//    Route::get('/{idea}/edit', [IdeaController::class, 'edit'])->name('edit');
+//    Route::put('/{idea}/update', [IdeaController::class, 'update'])->name('update');
+//    Route::delete('/{idea}', [IdeaController::class, 'destroy'])->name('destroy');
+//    Route::post('/{idea}/comments', [CommentController::class, 'store'])->name('comment.store');
+//});
+
+Route::resource('ideas', IdeaController::class)->except(['index', 'create', 'show'])->middleware('auth');
+Route::resource('ideas', IdeaController::class)->only(['show']);
+
+Route::resource('ideas.comments', CommentController::class)->only(['store'])->middleware('auth');
 
 
 Route::get('/terms', function () {
