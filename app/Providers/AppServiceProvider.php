55e633bb9a59b44c6d9911fdeb09c4989bf5f8aa
khiemdev <khiembi161219@gmail.com>
2024-07-02 10:11:03 +0700

    Caching

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\User;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -24,9 +25,15 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrapFive();
 
+        Cache::forget('topUsers');
+
+        $topUsers = Cache::remember('topUsers', now()->addMinutes(5), function () {
+            return User::withCount('ideas')->orderBy('ideas_count', 'desc')->take(5)->get();
+        });
+
         View::share(
             'topUsers',
-            User::withCount('ideas')->orderBy('ideas_count', 'desc')->take(5)->get()
+            $topUsers
         );
     }
 }
